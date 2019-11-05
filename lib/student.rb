9eb7a358541b9a825af56af84df4762a4525cd99
David Jaworski <jaws360@gmail.com>
2019-11-05 11:02:39 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+  
+  attr_accessor :first_name, :last_name 
 
+  def initialize
+    super
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge.push(string)
+  end
+
+  def knowledge
+    return @knowledge
+  end
 end
